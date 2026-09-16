38cfa8a95bc971bce8af7d8af4185897156c6607
anuroopa131 <anuroopa131@gmail.com>
2026-09-16 15:54:58 +0530

    Hardcode dynamic baseURL for production deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: (process.env.REACT_APP_API_URL || 'https://bunkmaster-gs92.onrender.com') + '/api',
+    baseURL: 'https://bunkmaster-gs92.onrender.com/api',
 });
 
 /**
